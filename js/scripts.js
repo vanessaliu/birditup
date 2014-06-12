b42029e0b145fa4b47fa2dbc0867c68cb763111b
laurakantti <laura.kantti@gmail.com>
2014-06-12 23:58:25 +0200

    0.1.2
    Laura: updates with modal, carousel, instagram-feed, responsive, css
    structuring, loadVotedImage, content etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 var birdItUp = {
 
+    template: '<div class="grid-image col-xs-12 col-sm-4 col-md-3"><h4>{{model.user.username}}</h4><a target="_blank" href="{{link}}"><img src="{{image}}" /></a><a class="fb-like" data-href="?vote={{id}}" data-width="200px" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></a></div>',
+
     init: function() {
-        // initialize all functions inside the object
+        // Instagram
         birdItUp.instaFeedBase();
         birdItUp.instaFeedFull();
+
+        birdItUp.loadVotedImage();
+
+        // Page functions
+        birdItUp.mobileMenu();
+
+        birdItUp.swapModalImage();
+
     },
 
     instaFeedBase: function() {
+        // how to get name?
         var feed = new Instafeed({
             get: 'tagged',
             tagName: 'bird',
             clientId: '9cd60ab846f743fcbc7a95d4c058dcc4',
-            resolution: 'low_resolution',
-            limit: 4,
+            resolution: 'thumbnail',
+            limit: 12,
             sortBy: 'most-recent',
             target: 'feed',
-            template: '<div class="grid-image col-xs-6 col-sm-6 col-md-4 col-lg-3"><a target="_blank" href="{{link}}"><img src="{{image}}" /></a><a class="fb-like" data-href="{{link}}" data-width="200px" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></a></div>',
+            template: this.template
         });
+        console.log('feed', feed);
         feed.run();
     },
 
@@ -27,10 +39,10 @@ var birdItUp = {
             tagName: 'bird',
             clientId: '9cd60ab846f743fcbc7a95d4c058dcc4',
             resolution: 'thumbnail',
-            limit: 32,
+            limit: 24,
             sortBy: 'most-recent',
             target: 'feed-full',
-            template: '<div class="grid-image col-xs-6 col-sm-6 col-md-4 col-lg-3"><a target="_blank" href="{{link}}"><img src="{{image}}" /></a><a class="fb-like" data-href="{{link}}" data-width="200px" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></a></div>',
+            template: this.template,
             after: function() {
             // disable button if no more results to load
                 if (!this.hasNext()) {
@@ -45,13 +57,62 @@ var birdItUp = {
         largeFeed.run();
     },
 
-    headerEffect: function() {
-        // Laura: header-effect med scrolling
+    loadVotedImage: function() {
+        var scope = this;
+        var mediaId;
+        mediaId = window.location.search.substring(6);
+        console.log(mediaId.length);
+        var clientId = '9cd60ab846f743fcbc7a95d4c058dcc4';
+        var url = 'https://api.instagram.com/v1/media/' + mediaId + '?client_id=' + clientId;
+        if (mediaId.length > 0) {
+            $.ajax({
+               type: 'GET',
+                url: url,
+                async: false,
+                jsonpCallback: 'jsonCallback',
+                contentType: "application/json",
+                dataType: 'jsonp',
+                success: function(data) {
+                    console.log('data', data);
+                    var newTemplate = scope.template;
+                    newTemplate = newTemplate.replace('{{image}}', data.data.images.low_resolution.url);
+                    newTemplate = newTemplate.replace('{{link}}', data.data.link);
+                    newTemplate = newTemplate.replace('{{id}}', data.data.id);
+                    newTemplate = newTemplate.replace('{{model.user.username}}', data.data.user.username);
+                    var image = $(newTemplate);
+                    image.appendTo($('body'));
+                    //
+                },
+                error: function(e) {
+                   console.log(e.message);
+                }
+            });
+        }
     },
 
     mobileMenu: function() {
-        // Laura: responsiv menu
+        $('.mobile-link').on('click', function() {
+            $('.mobile-menu').slideToggle();
+            $('.mobile-menu').toggleClass('nav-visible');
+        });
+        $('body').on('click', function(event) {
+            var notMenuIcon = ($(event.target).closest('.mobile-link').length === 0);
+            var notMobileMenu = ($(event.target).closest('.mobile-menu').length === 0);
+            var mobileMenuIsVisible = $('.mobile-menu').hasClass('nav-visible');
+            if (notMenuIcon && notMobileMenu && mobileMenuIsVisible) {
+                $('.mobile-menu').slideUp();
+                $('.mobile-menu').removeClass('nav-visible');
+            }
+        });
+    },
+
+    swapModalImage: function(image) {
+        $("main").src = image.href;
+
     }
+
+
+
 };
 
 birdItUp.init();
